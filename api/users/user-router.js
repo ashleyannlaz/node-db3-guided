1e2f455c698c41efcc7ea7d22be98d4d61a77ea1
Dave Kidd <dave@davekidd.net>
2021-08-23 19:09:23 -0600

    wip

--- api/users/user-router.js
+++ api/users/user-router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const db = require("../../data/db-config.js");
-
+const User = require("./user-model.js")
 const router = express.Router();
 
 router.get("/", (req, res) => {
