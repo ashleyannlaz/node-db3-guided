2b0484ef1d85410331456098ff53393648347e82
Dave Kidd <dave@davekidd.net>
2021-08-23 19:14:47 -0600

    wip

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -4,5 +4,7 @@ module.exports = {
     getUserPosts(id){
         return db("users u")
             .join("posts p","p.user_id","u.id")
+            .select("p.id","u.username","p.contents")
+            .where("u.id",id)
     }
 }
