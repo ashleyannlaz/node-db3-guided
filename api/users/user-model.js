72321e10e039f5d782ab064b8db4122b1efc7839
Dave Kidd <dave@davekidd.net>
2021-08-23 19:13:42 -0600

    wip

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -2,6 +2,7 @@ const db = require("../../data/db-config.js")
 
 module.exports = {
     getUserPosts(id){
-        
+        return db("users u")
+            .join("posts p","p.user_id","u.id")
     }
 }
